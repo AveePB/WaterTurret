5c6ed7c9cb7dc8863a763dfb43c0342735c65156
AveePB <mike303.py@gmail.com>
2023-12-20 16:03:12 +0100

    Changed file structures

--- src/main/java/dev/aveepb/diary/security/filter/JwtAuthenticationFilter.java
+++ src/main/java/dev/aveepb/diary/security/filter/JwtAuthenticationFilter.java
@@ -1,2 +1,63 @@
-package dev.aveepb.diary.security.filter;public class JwtAuthenticationFilter {
+package dev.aveepb.diary.security.filter;
+
+import dev.aveepb.diary.security.service.JwtService;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Optional;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private static final String PREFIX = "Bearer ";
+
+    private final JwtService jwtService;
+    private final UserService userService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authHeader = request.getHeader("Authorization");
+
+        //Validate request form.
+        if (authHeader != null && authHeader.startsWith(PREFIX)) {
+
+            //Extract important data from raw request.
+            String token = authHeader.substring(PREFIX.length());
+            Optional<String> username = this.jwtService.fetchUsername(token);
+            Optional<String> password = this.jwtService.fetchUserPassword(token);
+
+            //Check user authentication status.
+            if ((username.isPresent() && password.isPresent()) && SecurityContextHolder.getContext().getAuthentication() == null) {
+                UserDetails user = this.userService.loadUserByUsernameAndPassword(username.get(), password.get());
+
+                //Check user token.
+                if (this.jwtService.isTokenValid(token, user)) {
+
+                    //Create authentication for current request.
+                    UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+                    authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+                    //Update security context.
+                    SecurityContextHolder.getContext().setAuthentication(authentication);
+                }
+            }
+        }
+
+        //Run next filter.
+        filterChain.doFilter(request, response);
+    }
 }
--- src/main/java/dev/aveepb/diary/security/service/JwtService.java
+++ src/main/java/dev/aveepb/diary/security/service/JwtService.java
@@ -1,8 +1,8 @@
 package dev.aveepb.diary.security.service;
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import dev.aveepb.diary.security.utils.JsonWebToken;
+import dev.aveepb.diary.security.utils.JsonWebTokenClaims;
+
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 
@@ -10,114 +10,47 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.function.Function;
+import java.util.Optional;
 
 @Service
 public class JwtService {
 
     private static final String SIGNING_KEY = "";
 
-    //PRIVATE:
+    /**
+     * @param token the json web token.
+     * @param user the user details.
+     * @return true if token is valid otherwise false.
+     */
+    public boolean isTokenValid(String token, UserDetails user) {
+
+        return JsonWebToken.isTokenValid(token, fetchSigningKey(), user);
+    }
 
     /**
-     * @return SIGNING_KEY in the form of Key object
+     * @param token the json web token.
+     * @return the username name.
      */
-    private Key getSigningKey() {
+    public Optional<String> fetchUsername(String token) {
+
+        return Optional.of(JsonWebTokenClaims.extractUsername(token, fetchSigningKey()));
+    }
+
+    /**
+     * @param token the json web token.
+     * @return the user password.
+     */
+    public Optional<String> fetchUserPassword(String token) {
+
+        return Optional.of(JsonWebTokenClaims.extractPassword(token, fetchSigningKey()));
+    }
+
+    /**
+     * @return SIGNING_KEY in the form of Key object.
+     */
+    public Key fetchSigningKey() {
         byte[] decodedKey = Decoders.BASE64.decode(SIGNING_KEY);
 
         return Keys.hmacShaKeyFor(decodedKey);
     }
-
-    /**
-     * @param token the json web token.
-     * @param func the function parameter.
-     * @return the claim.
-     * @param <T> the type of function result.
-     */
-    private <T> T extractClaim(String token, Function<Claims, T> func) {
-        Claims claims = extractAllClaims(token);
-
-        return func.apply(claims);
-    }
-
-    /**
-     * @param token the json web token.
-     * @return the claims.
-     */
-    private Claims extractAllClaims(String token) {
-
-        return Jwts.parserBuilder()
-                .setSigningKey(getSigningKey())
-                .build()
-                .parseClaimsJws(token)
-                .getBody();
-    }
-
-    /**
-     * @param token the json web token.
-     * @return the expiration date.
-     */
-    private Date extractExpirationDate(String token) {
-
-        return extractClaim(token, Claims::getExpiration);
-    }
-
-    /**
-     * @param token the json web token.
-     * @return true if token is expired otherwise false.
-     */
-    private boolean isTokenExpired(String token) {
-
-        return extractExpirationDate(token).before(new Date());
-    }
-
-    //PUBLIC:
-
-    /**
-     * @param token the json web token.
-     * @return the username.
-     */
-    public String extractUsername(String token) {
-
-        return extractClaim(token, Claims::getSubject);
-    }
-
-    /**
-     * @param userDetails the user details.
-     * @return the new token.
-     */
-    public String generateToken(UserDetails userDetails) {
-
-        return generateToken(new HashMap<>(), userDetails);
-    }
-
-    /**
-     * @param extraClaims the hash map storing additional claims.
-     * @param userDetails the user details.
-     * @return the new token.
-     */
-    public String generateToken(HashMap<String, Object> extraClaims, UserDetails userDetails) {
-
-        return Jwts.builder()
-                .setClaims(extraClaims)
-                .setSubject(userDetails.getUsername())
-                .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000*60))
-                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
-                .compact();
-    }
-
-    /**
-     * @param token the json web token.
-     * @param userDetails the user details.
-     * @return true if token is valid otherwise false.
-     */
-    public boolean isTokenValid(String token, UserDetails userDetails) {
-        String username = extractUsername(token);
-
-        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
-    }
-
 }

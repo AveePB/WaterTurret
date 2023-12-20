4e308af38039780229b3e0c29c492fb0d8fa6be3
AveePB <mike303.py@gmail.com>
2023-12-20 16:02:38 +0100

    Create two util files

--- src/main/java/dev/aveepb/diary/security/utils/JsonWebToken.java
+++ src/main/java/dev/aveepb/diary/security/utils/JsonWebToken.java
@@ -1,2 +1,63 @@
-package dev.aveepb.diary.security.utils;public class JsonWebToken {
+package dev.aveepb.diary.security.utils;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+
+public class JsonWebToken {
+
+    /**
+     * @param token the json web token.
+     * @param key the signing key.
+     * @return true if token is expired otherwise false.
+     */
+    public static boolean isTokenExpired(String token, Key key) {
+
+        return JsonWebTokenClaims.extractExpirationDate(token, key).before(new Date());
+    }
+
+    /**
+     * @param token the json web token.
+     * @param key the signing key.
+     * @param user the user details.
+     * @return true if token is valid otherwise false.
+     */
+    public static boolean isTokenValid(String token, Key key, UserDetails user) {
+        String username = JsonWebTokenClaims.extractUsername(token, key);
+
+        return (username.equals(user.getUsername()) && !isTokenExpired(token, key));
+    }
+
+    /**
+     * @param key the signing key.
+     * @param user the user details.
+     * @return the new token.
+     */
+    public static String generateToken(Key key, UserDetails user) {
+
+        return generateToken(new HashMap<>(), key, user);
+    }
+
+    /**
+     * @param extraClaims the hash map storing additional claims.
+     * @param key the signing key.
+     * @param user the user details.
+     * @return the new token.
+     */
+    public static String generateToken(HashMap<String, Object> extraClaims, Key key, UserDetails user) {
+        extraClaims.put("pass", user.getPassword());
+
+        return Jwts.builder()
+                .setClaims(extraClaims)
+                .setSubject(user.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000*60))
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
 }
--- src/main/java/dev/aveepb/diary/security/utils/JsonWebTokenClaims.java
+++ src/main/java/dev/aveepb/diary/security/utils/JsonWebTokenClaims.java
@@ -1,2 +1,57 @@
-package dev.aveepb.diary.security.utils;public class JsonWebTokenClaims {
+package dev.aveepb.diary.security.utils;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+
+import java.security.Key;
+import java.util.Date;
+
+public class JsonWebTokenClaims {
+
+    /**
+     * @param token the json web token.
+     * @param key the signing key.
+     * @return the claim.
+     */
+    public static String extractUsername(String token, Key key) {
+        Claims claims = extractAllClaims(token, key);
+
+        return claims.getSubject();
+    }
+
+    /**
+     * @param token the json web token.
+     * @param key the signing key.
+     * @return the claim.
+     */
+    public static String extractPassword(String token, Key key) {
+        Claims claims = extractAllClaims(token, key);
+
+        return claims.get("pass", String.class);
+    }
+
+    /**
+     * @param token the json web token.
+     * @param key the signing key.
+     * @return the claim.
+     */
+    public static Date extractExpirationDate(String token, Key key) {
+        Claims claims = extractAllClaims(token, key);
+
+        return claims.getExpiration();
+    }
+
+    /**
+     * @param token the json web token.
+     * @param key the signing key.
+     * @return the claims.
+     */
+    public static Claims extractAllClaims(String token, Key key) {
+
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
 }
